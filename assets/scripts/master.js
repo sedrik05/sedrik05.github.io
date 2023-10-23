6dab23a8f29cc95d74d26a10289457866def942f
SEDRIK JOHN TORQUATOR <torquas@ph.ibm.com>
2023-10-24 01:36:47 +0800

    Updated About Me Page

--- assets/scripts/master.js
+++ assets/scripts/master.js
@@ -1,6 +1,7 @@
 const sedPortfolio = {
 	init: () => {
 		sedPortfolio.loadNavFunc();
+		sedPortfolio.loadSkillsFunc();
 	},
 	loadNavFunc: () => {
 		$('nav a').on('click', function(e){
@@ -16,6 +17,43 @@ const sedPortfolio = {
 			$('#container > div'+ targetDiv).addClass('active').show();
 		});
 	},
+	loadSkillsFunc: () => {
+		var numberOfStars = [];
+
+		$('#about-me-container .skills h4').each(function(){
+			var _this = $(this),
+				rating = parseFloat(_this.data('rate')),
+				half = 0,
+				full = 0,
+				none = 5 - rating;
+
+			if(!Number.isInteger(rating)){
+				full = String(rating).split('.')[0];
+				half = 1;
+				none = 5 - (parseInt(full) + 1);
+			} else {
+				full = rating;
+			}
+
+			if(full){
+				for(var x = 0; x < full; x++){
+					_this.find('span').append(' <i class="fa-solid fa-star"></i>');
+				}
+			}
+
+			if(half){
+				for(var x = 0; x < half; x++){
+					_this.find('span').append(' <i class="fa-solid fa-star-half-stroke"></i>');
+				}
+			}
+
+			if(none){
+				for(var x = 0; x < none; x++){
+					_this.find('span').append(' <i class="fa-regular fa-star"></i>');
+				}
+			}
+		});
+	}
 };
 
 $(document).ready(function(){
